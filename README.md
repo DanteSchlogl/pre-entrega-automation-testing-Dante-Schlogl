15ae5f1e766eca9b2f1dd67c48c2ff208fe7fa8c
Dante Schlogl <dante_schlogl@hotmail.com>
2025-10-12 13:35:32 -0300

    Initial commit of project files

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+Tecnologías Requeridas
+
+Python como lenguaje principal
+
+Pytest para estructura de testing
+
+Selenium WebDriver para automatización
+
+Git y GitHub para control de versiones
+
+Pytest HTML
---
+++ test/test_saucedemo.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+
+
+@pytest.fixture
+  
+
+def  test_login():
+
+    # logeo de  usuario  con username y pass
+    #click boton de login
+
+    #rediriga a la pagina de inventario
+    #vereficar el titulo de la pagina
+
+
+
+def test_catalogo():
+
+#logeo de usuario con username y pass
+#click boton de login
+
+#podamos verificar el titulo pero del html
+
+#comprobare si existen productos en la pagina visibles (usando len())
+#verifica si hay titulo, parrafo o productos (opcional)
+#verificar elementos importantes de la pagina
+
+
+def test_carrito(): 
+
+# logeo de  usuario  con username y pass
+#click boton de login
+
+# levarme a la pagina de carrito de compras
+#verificar incremento del carrito al agrgar un producto
+
+#comprobar que el carrito aparezca correctamente el             
